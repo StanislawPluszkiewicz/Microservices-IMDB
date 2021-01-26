b05cadd70315316ec7a512f0ded593a08596800c
Stanislaw PLUSZKIEWICZ <stanislaw.karol.pluszkiewicz@gmail.com>
2021-01-26 11:56:22 +0100

    Added colored logs to db_utils.py+

--- backend/db_utils.py
+++ backend/db_utils.py
@@ -1,4 +1,4 @@
-import logging
+import coloredlogs, logging
 from pymongo import monitoring
 
 from mongoengine import (
@@ -8,25 +8,26 @@ from mongoengine import (
 import datetime
 
 
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+logger = logging.getLogger('')
+logger.setLevel(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
+coloredlogs.install(level='DEBUG', logger=logger)
 
 class CommandLogger(monitoring.CommandListener):
 
     def started(self, event):
-        log.debug("Command {0.command_name} with request id "
+        logger.debug("Command {0.command_name} with request id "
                  "{0.request_id} started on server "
                  "{0.connection_id}".format(event))
 
     def succeeded(self, event):
-        log.debug("Command {0.command_name} with request id "
+        logger.info("Command {0.command_name} with request id "
                  "{0.request_id} on server {0.connection_id} "
                  "succeeded in {0.duration_micros} "
                  "microseconds".format(event))
 
     def failed(self, event):
-        log.debug("Command {0.command_name} with request id "
+        logger.warn("Command {0.command_name} with request id "
                  "{0.request_id} on server {0.connection_id} "
                  "failed in {0.duration_micros} "
                  "microseconds".format(event))
